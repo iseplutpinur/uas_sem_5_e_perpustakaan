c9f73501390019643770c09673bebe6612e8d50f
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-01-06 06:03:04 +0700

    update database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            RoleSeeder::class,
-            UserSeeder::class,
-            KategoriSeeder::class,
-            RakSeeder::class,
-            PenerbitSeeder::class,
-        ]);;
+        // php artisan iseed users,password_resets,failed_jobs,personal_access_tokens,kategori,rak,penerbit,buku,peminjaman,detail_peminjaman,sessions,posts --force
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(PasswordResetsTableSeeder::class);
+        $this->call(FailedJobsTableSeeder::class);
+        $this->call(PersonalAccessTokensTableSeeder::class);
+        $this->call(KategoriTableSeeder::class);
+        $this->call(RakTableSeeder::class);
+        $this->call(PenerbitTableSeeder::class);
+        $this->call(BukuTableSeeder::class);
+        $this->call(PeminjamanTableSeeder::class);
+        $this->call(DetailPeminjamanTableSeeder::class);
+        $this->call(SessionsTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
     }
 }
